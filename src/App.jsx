ae99f1999798dda8b8769cad596ea82753ab3159
Ross Tungol <ross@proudcloud.io>
2023-10-23 22:33:55 +0800

    Adds Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react"
-
-import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Movie Browser</h1>
+      <Navbar />
     </>
   )
 }
