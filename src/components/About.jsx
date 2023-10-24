9db2e4219a60786b0523a6ffdfec5d311a3fa55f
Ross Tungol <ross@proudcloud.io>
2023-10-25 04:44:10 +0800

    Adds generic content to Home and About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,18 @@ const About = () => {
   return (
     <>
       <Hero text='About us' />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-8 offset-lg-2 my-5'>
+            <p className='lead'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+              consequatur accusantium nam deleniti cupiditate illum, voluptatum
+              accusamus deserunt, facilis perferendis tempora dolorem harum
+              laudantium quaerat atque placeat incidunt, voluptate inventore.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,18 @@ const Home = () => {
   return (
     <>
       <Hero text='Welcome' />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-8 offset-lg-2 my-5'>
+            <p className='lead'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+              sequi nemo labore natus, autem id tempora nisi error consectetur
+              voluptatem, doloremque accusantium eaque voluptate vitae illum
+              recusandae distinctio culpa maiores?
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
