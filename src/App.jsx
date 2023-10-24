48db017cd194f439f2fef2b4c963a0bc859e7899
Ross Tungol <ross@proudcloud.io>
2023-10-24 23:49:25 +0800

    Adds Routes from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   )
 }
