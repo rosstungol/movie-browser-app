88749f40681dbdf9375dc3b048a0ca6d78cf9f6d
Ross Tungol <ross@proudcloud.io>
2023-10-26 08:45:10 +0800

    Adds Search component and Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import About from "./components/About"
+import Search from "./components/Search"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
     </>
   )
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero"
+
+const Search = () => {
+  return (
+    <>
+      <Hero text='You are searching for...' />
+    </>
+  )
+}
+
+export default Search
