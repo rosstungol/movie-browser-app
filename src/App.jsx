90b09327187530c72e2dda42b2827e4c2d07eea3
Ross Tungol <ross@proudcloud.io>
2023-10-27 23:41:39 +0800

    Adds fetch for movie search results

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import About from "./components/About"
 import Search from "./components/Search"
+import { getMovieResultsUrl } from "./utils/utils"
 
 function App() {
   const [searchResults, setSearchResults] = useState([])
   const [searchText, setSearchText] = useState("")
+
+  useEffect(() => {
+    if (searchText) {
+      fetch(getMovieResultsUrl(searchText))
+        .then((response) => response.json())
+        .then((data) => setSearchResults(data.results))
+    }
+  }, [searchText])
 
   return (
     <>
