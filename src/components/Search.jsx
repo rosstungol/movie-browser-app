8c133836169c92c16a59d65e6e00f69f45755c1f
Ross Tungol <ross@proudcloud.io>
2023-10-27 23:44:35 +0800

    Adds rendering for movie search results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,20 @@
 import Hero from "./Hero"
+import MovieCard from "./MovieCard"
 
 const Search = ({ keyword, searchResults }) => {
   const title = `You are searching for ${keyword}`
+  const resultsList = searchResults.map((movie) => {
+    return <MovieCard movie={movie} key={movie.id} />
+  })
 
   return (
     <>
       <Hero text={title} />
+      {resultsList && (
+        <div className='container'>
+          <div className='row'>{resultsList}</div>
+        </div>
+      )}
     </>
   )
 }
