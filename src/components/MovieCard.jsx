720a6592256f703d1d657ce6dd98ac5bf84cdff6
Ross Tungol <ross@proudcloud.io>
2023-10-29 22:57:26 +0800

    Adds detail Url to MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom"
+
 const MovieCard = ({ movie }) => {
   const posterURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+  const detailUrl = `/movies/${movie.id}`
 
   return (
     <div className='col-lg-3 col-md-3 col-2 my-4'>
@@ -11,10 +14,9 @@ const MovieCard = ({ movie }) => {
         />
         <div className='card-body'>
           <h5 className='card-title'>{movie.original_title}</h5>
-          {/* <p className='card-text'>{movie.overview}</p> */}
-          <a href='#' className='btn btn-primary'>
+          <Link to={detailUrl} className='btn btn-primary'>
             Show details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
