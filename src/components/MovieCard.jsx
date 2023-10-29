44e91c90ef81beba9761327620097827b15c2004
Ross Tungol <ross@proudcloud.io>
2023-10-30 02:06:11 +0800

    Adds blank image for empty poster image in MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import blankMovieImage from "../assets/blank-movie-image.jpg"
 
 const MovieCard = ({ movie }) => {
   const posterURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
@@ -7,11 +8,20 @@ const MovieCard = ({ movie }) => {
   return (
     <div className='col-lg-3 col-md-3 col-2 my-4'>
       <div className='card'>
-        <img
-          src={posterURL}
-          className='card-img-top'
-          alt={movie.original_title}
-        />
+        {movie.poster_path ? (
+          <img
+            src={posterURL}
+            className='card-img-top'
+            alt={movie.original_title}
+          />
+        ) : (
+          <img
+            src={blankMovieImage}
+            className='card-img-top'
+            alt={movie.original_title}
+          />
+        )}
+
         <div className='card-body'>
           <h5 className='card-title'>{movie.original_title}</h5>
           <Link to={detailUrl} className='btn btn-primary'>
