f45fae13e542feea17d20d96485bd06b4021c0d7
Ross Tungol <ross@proudcloud.io>
2023-10-29 23:08:15 +0800

    Adds Loading state for rendering details

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,22 +5,28 @@ import Hero from "./Hero"
 const Movie = () => {
   const { id } = useParams()
   const [movieDetails, setMovieDetails] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    console.log("make an api request", id)
     fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=f112456204d06b0cd5c500363b2e0cd0&languange=en-US`
     )
       .then((response) => response.json())
       .then((data) => {
         setMovieDetails(data)
+        setIsLoading(false)
       })
   }, [id])
-  return (
-    <>
-      <Hero text={movieDetails.original_title} />
-    </>
-  )
+
+  function renderMovieDetails() {
+    if (isLoading) {
+      return <Hero text='Loading...' />
+    }
+    if (movieDetails) {
+      return <Hero text={movieDetails.original_title} />
+    }
+  }
+  return renderMovieDetails()
 }
 
 export default Movie
