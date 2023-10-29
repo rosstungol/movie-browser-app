d7f8667b00858fb78f6b7c1cb1a5d243f482ba44
Ross Tungol <ross@proudcloud.io>
2023-10-30 01:26:40 +0800

    Adds backdrop for Hero component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -24,9 +24,10 @@ const Movie = () => {
     }
     if (movieDetails) {
       const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+      const backdropUrl = `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
       return (
         <>
-          <Hero text={movieDetails.original_title} />
+          <Hero text={movieDetails.original_title} backdrop={backdropUrl} />
           <div className='container my-5'>
             <div className='row'>
               <div className='col-md-3'>
