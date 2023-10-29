729755096fbe34f72c2e744683fd4ee61448a3ef
Ross Tungol <ross@proudcloud.io>
2023-10-30 01:00:48 +0800

    Adds movie details in Movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -23,7 +23,27 @@ const Movie = () => {
       return <Hero text='Loading...' />
     }
     if (movieDetails) {
-      return <Hero text={movieDetails.original_title} />
+      const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+      return (
+        <>
+          <Hero text={movieDetails.original_title} />
+          <div className='container my-5'>
+            <div className='row'>
+              <div className='col-md-3'>
+                <img
+                  src={posterPath}
+                  alt='...'
+                  className='img-fluid shadow rounded'
+                />
+              </div>
+              <div className='col-md-9'>
+                <h2>{movieDetails.original_title}</h2>
+                <p className='lead'>{movieDetails.overview}</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )
     }
   }
   return renderMovieDetails()
