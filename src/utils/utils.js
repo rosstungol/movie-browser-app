a7f19e58ffc6def6a4f6b306d6fed9aa0e9484b9
Ross Tungol <ross@proudcloud.io>
2023-10-29 22:04:49 +0800

    Refactors getMovieResultsUrl function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
-import { apiKey } from "../data/constants"
-
 export const getMovieResultsUrl = (query) => {
+  const apiKey = "f112456204d06b0cd5c500363b2e0cd0"
+
   return (
     "https://api.themoviedb.org/3/search/movie?" +
     "api_key=" +
